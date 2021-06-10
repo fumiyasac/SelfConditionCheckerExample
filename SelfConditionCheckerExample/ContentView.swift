7498b48600bf20596e1600e2303ead5ed0cd0371
Fumiya Sakai <just1factory@gmail.com>
2021-06-10 20:19:41 +0900

    initial commit

---
+++ SelfConditionCheckerExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SelfConditionCheckerExample
+//
+//  Created by 酒井文也 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SelfConditionCheckerExample/SelfConditionCheckerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfConditionCheckerExampleApp.swift
+//  SelfConditionCheckerExample
+//
+//  Created by 酒井文也 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct SelfConditionCheckerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
